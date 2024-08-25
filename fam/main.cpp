f9f7786aa073028d6e7a6ac80724b8a603fe2010
Shaoze Yang <shaozey@umich.edu>
2024-08-25 23:26:43 +0800

    debug csv parser to automaticaly match column

--- fam/main.cpp
+++ fam/main.cpp
@@ -54,70 +54,54 @@ struct Features {
     double User_Y;
     int TimestampID;
 
+//     // Maps field names to setter functions
     std::unordered_map<std::string, std::function<void(const std::string&)>> setters;
 
     Features() {
-        setters["AGV_distance_X"] = [this](const std::string& val) { AGV_distance_X = std::stod(val); };
-        // Initialize other setters similarly...
+        setters["User_speed_X"] = [this](const std::string& val) { User_speed_X = std::stod(val); };
+        setters["User_speed_Y"] = [this](const std::string& val) { User_speed_Y = std::stod(val); };
+        setters["User_speed"] = [this](const std::string& val) { User_speed = std::stod(val); };
+        setters["User_velocity_X"] = [this](const std::string& val) { User_velocity_X = std::stod(val); };
+        setters["User_velocity_Y"] = [this](const std::string& val) { User_velocity_Y = std::stod(val); };
+        setters["Wait_time"] = [this](const std::string& val) { Wait_time = std::stod(val); };
+        setters["intent_to_cross"] = [this](const std::string& val) { intent_to_cross = val == "True"; };
+        setters["Gazing_station"] = [this](const std::string& val) { Gazing_station = std::stoi(val); };
+        setters["possible_interaction"] = [this](const std::string& val) { possible_interaction = val == "True"; };
+        setters["facing_along_sidewalk"] = [this](const std::string& val) { facing_along_sidewalk = val == "True"; };
+        setters["facing_to_road"] = [this](const std::string& val) { facing_to_road = val == "True"; };
+        setters["On_sidewalks"] = [this](const std::string& val) { On_sidewalks = val == "True"; };
+        setters["On_road"] = [this](const std::string& val) { On_road = val == "True"; };
+        setters["closest_station"] = [this](const std::string& val) { closest_station = std::stoi(val); };
+        setters["distance_to_closest_station"] = [this](const std::string& val) { distance_to_closest_station = std::stod(val); };
+        setters["distance_to_closest_station_X"] = [this](const std::string& val) { distance_to_closest_station_X = std::stod(val); };
+        setters["distance_to_closest_station_Y"] = [this](const std::string& val) { distance_to_closest_station_Y = std::stod(val); };
         setters["looking_at_AGV"] = [this](const std::string& val) { looking_at_AGV = val == "True"; };
+        setters["start_station_X"] = [this](const std::string& val) { start_station_X = std::stod(val); };
+        setters["start_station_Y"] = [this](const std::string& val) { start_station_Y = std::stod(val); };
+        setters["end_station_X"] = [this](const std::string& val) { end_station_X = std::stod(val); };
+        setters["end_station_Y"] = [this](const std::string& val) { end_station_Y = std::stod(val); };
+        setters["distance_from_start_station_X"] = [this](const std::string& val) { distance_from_start_station_X = std::stod(val); };
+        setters["distance_from_start_station_Y"] = [this](const std::string& val) { distance_from_start_station_Y = std::stod(val); };
+        setters["distance_from_end_station_X"] = [this](const std::string& val) { distance_from_end_station_X = std::stod(val); };
+        setters["distance_from_end_station_Y"] = [this](const std::string& val) { distance_from_end_station_Y = std::stod(val); };
+        setters["facing_start_station"] = [this](const std::string& val) { facing_start_station = val == "True"; };
+        setters["facing_end_station"] = [this](const std::string& val) { facing_end_station = val == "True"; };
+        setters["looking_at_closest_station"] = [this](const std::string& val) { looking_at_closest_station = val == "True"; };
+        setters["GazeDirection_X"] = [this](const std::string& val) { GazeDirection_X = std::stod(val); };
+        setters["GazeDirection_Y"] = [this](const std::string& val) { GazeDirection_Y = std::stod(val); };
+        setters["AGV_X"] = [this](const std::string& val) { AGV_X = std::stod(val); };
+        setters["AGV_Y"] = [this](const std::string& val) { AGV_Y = std::stod(val); };
+        setters["User_X"] = [this](const std::string& val) { User_X = std::stod(val); };
+        setters["User_Y"] = [this](const std::string& val) { User_Y = std::stod(val); };
+        setters["TimestampID"] = [this](const std::string& val) { TimestampID = std::stoi(val); };
     }
 
     void setField(const std::string& name, const std::string& value) {
         if (setters.find(name) != setters.end()) {
-            std::cout << "name: " << name << " value: " << value << std::endl;
             setters[name](value);
         }
     }
 };
-
-//     // Maps field names to setter functions
-//     std::unordered_map<std::string, std::function<void(const std::string&)>> setters;
-
-//     Features() {
-//         setters["User_speed_X"] = [this](const std::string& val) { User_speed_X = std::stod(val); };
-//         setters["User_speed_Y"] = [this](const std::string& val) { User_speed_Y = std::stod(val); };
-//         setters["User_speed"] = [this](const std::string& val) { User_speed = std::stod(val); };
-//         setters["User_velocity_X"] = [this](const std::string& val) { User_velocity_X = std::stod(val); };
-//         setters["User_velocity_Y"] = [this](const std::string& val) { User_velocity_Y = std::stod(val); };
-//         setters["Wait_time"] = [this](const std::string& val) { Wait_time = std::stod(val); };
-//         setters["intent_to_cross"] = [this](const std::string& val) { intent_to_cross = val == "true"; };
-//         setters["Gazing_station"] = [this](const std::string& val) { Gazing_station = std::stoi(val); };
-//         setters["possible_interaction"] = [this](const std::string& val) { possible_interaction = val == "true"; };
-//         setters["facing_along_sidewalk"] = [this](const std::string& val) { facing_along_sidewalk = val == "true"; };
-//         setters["facing_to_road"] = [this](const std::string& val) { facing_to_road = val == "true"; };
-//         setters["On_sidewalks"] = [this](const std::string& val) { On_sidewalks = val == "true"; };
-//         setters["On_road"] = [this](const std::string& val) { On_road = val == "true"; };
-//         setters["closest_station"] = [this](const std::string& val) { closest_station = std::stoi(val); };
-//         setters["distance_to_closest_station"] = [this](const std::string& val) { distance_to_closest_station = std::stod(val); };
-//         setters["distance_to_closest_station_X"] = [this](const std::string& val) { distance_to_closest_station_X = std::stod(val); };
-//         setters["distance_to_closest_station_Y"] = [this](const std::string& val) { distance_to_closest_station_Y = std::stod(val); };
-//         setters["looking_at_AGV"] = [this](const std::string& val) { looking_at_AGV = val == "true"; };
-//         setters["start_station_X"] = [this](const std::string& val) { start_station_X = std::stod(val); };
-//         setters["start_station_Y"] = [this](const std::string& val) { start_station_Y = std::stod(val); };
-//         setters["end_station_X"] = [this](const std::string& val) { end_station_X = std::stod(val); };
-//         setters["end_station_Y"] = [this](const std::string& val) { end_station_Y = std::stod(val); };
-//         setters["distance_from_start_station_X"] = [this](const std::string& val) { distance_from_start_station_X = std::stod(val); };
-//         setters["distance_from_start_station_Y"] = [this](const std::string& val) { distance_from_start_station_Y = std::stod(val); };
-//         setters["distance_from_end_station_X"] = [this](const std::string& val) { distance_from_end_station_X = std::stod(val); };
-//         setters["distance_from_end_station_Y"] = [this](const std::string& val) { distance_from_end_station_Y = std::stod(val); };
-//         setters["facing_start_station"] = [this](const std::string& val) { facing_start_station = val == "true"; };
-//         setters["facing_end_station"] = [this](const std::string& val) { facing_end_station = val == "true"; };
-//         setters["looking_at_closest_station"] = [this](const std::string& val) { looking_at_closest_station = val == "true"; };
-//         setters["GazeDirection_X"] = [this](const std::string& val) { GazeDirection_X = std::stod(val); };
-//         setters["GazeDirection_Y"] = [this](const std::string& val) { GazeDirection_Y = std::stod(val); };
-//         setters["AGV_X"] = [this](const std::string& val) { AGV_X = std::stod(val); };
-//         setters["AGV_Y"] = [this](const std::string& val) { AGV_Y = std::stod(val); };
-//         setters["User_X"] = [this](const std::string& val) { User_X = std::stod(val); };
-//         setters["User_Y"] = [this](const std::string& val) { User_Y = std::stod(val); };
-//         setters["TimestampID"] = [this](const std::string& val) { TimestampID = std::stoi(val); };
-//     }
-
-//     void setField(const std::string& name, const std::string& value) {
-//         if (setters.find(name) != setters.end()) {
-//             setters[name](value);
-//         }
-//     }
-// };
 
 
 
@@ -153,6 +137,7 @@ std::vector<Features> parseCSV(const std::string& filePath) {
         }
 
         records.push_back(features);
+        std::cout << features.User_speed_X << std::endl;
     }
 
     return records;
@@ -160,7 +145,7 @@ std::vector<Features> parseCSV(const std::string& filePath) {
 
 
 int main() {
-    std::string filePath = "/home/shaoze/Documents/Boeing/UE-integrated-pipeline/data/PandasData/Sampled_state/PID001_NSL.csv";
+    std::string filePath = "/home/shaoze/Documents/Boeing/UE-integrated-pipeline/data/demo/feature_fam/0.csv";
     auto featuresList = parseCSV(filePath);
 
     for (const auto& features : featuresList) {
